1d90c8fcd92008ed13096cc7f668717f193005d4
Paco Leyva <118618592+Fleyvad@users.noreply.github.com>
2023-03-01 21:30:44 +0100

    add test for app and logger files

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,3 +1,9 @@
-test('Given a app file', () => {
-  expect(true).toBe(true);
+import request from 'supertest';
+import app from './app';
+
+describe('Given an app', () => {
+  test('When the server then the app should respond with a mesage', async () => {
+    const res = await request(app).get('/');
+    expect(res.body).toEqual({ hello: 'World' });
+  });
 });
